336a90c2ef5649e0bfb55857f0eb0866072aa73f
Mayank3108 <58355558+Mayank3108@users.noreply.github.com>
2021-06-26 17:08:28 +0530

    Collections.namedtuple() file uploaded

--- Python Basic/Collections.namedtuple().py
+++ Python Basic/Collections.namedtuple().py
@@ -1,3 +1,17 @@
+# from collections import namedtuple
+# n, student = int(input()), namedtuple('student', ",".join(list(map(str, input().strip().split()))))
+# print (sum([float(s.MARKS) for s in [student(*input().strip().split()) for _ in range(n)]]) / n)
+
 from collections import namedtuple
-n, student = int(raw_input()), namedtuple('student', ",".join(list(map(str, raw_input().strip().split()))))
-print sum([float(s.MARKS) for s in [student(*raw_input().strip().split()) for _ in range(n)]]) / n
+n = int(input())
+columns  = input().split()
+sum = 0
+
+for i in range(n):
+    students = namedtuple('student', columns )
+    MARKS, CLASS, NAME, ID = input().split()
+    student = students(MARKS, CLASS, NAME, ID)
+    # sum += int(students.MARKS)
+    print(int(students.MARKS))
+
+# print('%.2f'%(sum/n))
