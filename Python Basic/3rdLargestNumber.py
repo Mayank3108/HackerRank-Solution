5f193839b4be2cd54aab065adb5779599140877a
Mayank3108 <58355558+Mayank3108@users.noreply.github.com>
2021-07-22 11:57:27 +0530

    Program for Third Largest Number in an Array

---
+++ Python Basic/3rdLargestNumber.py
@@ -0,0 +1,18 @@
+# data = list(map(int, input().split()))
+data = [8, 9, 15, 12, 14, 7, 9, 16, 22, 17, 22]
+data.sort()
+
+print (data[-3])
+
+
+# largest = max(data)
+# if largest != data[-2]:
+#     second = data[-2]
+#     if  second != data[-3]:
+#         third = data[-3]
+
+# n = data.count(max(data))
+# second = data[-(n+1)]
+# m= data.count(second)
+# third = data[-(n+m+1)]
+# print(third)
